07195d83ddff93a3ca1b9254398c89c74ee926c2
Anna Ö <anna.ostman24@gmail.com>
2024-01-24 13:22:35 +0200

    Add Spring Boot initialize files

---
+++ src/main/java/com/example/crosskey_codetest_spring/CrosskeyCodetestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.crosskey_codetest_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrosskeyCodetestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrosskeyCodetestSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crosskey_codetest_spring/CrosskeyCodetestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crosskey_codetest_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrosskeyCodetestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
